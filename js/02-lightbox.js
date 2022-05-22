25a206b1667aa23768a99aadac4c7ddae970e76c
AnastasiiaLAV <lav21051991@gmail.com>
2022-05-22 19:36:24 +0300

    add second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const refs = {
+    wrapGallery: document.querySelector(".gallery"),
+}
+
+const markupGalleryItems = createsPictureCards(galleryItems);
+
+refs.wrapGallery.insertAdjacentHTML('beforeend', markupGalleryItems)
+
+function createsPictureCards(items) {
+    return items.map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" 
+            title="${description}"/>
+            </a> `
+        })
+        .join('')
+}
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+})
+
+
+//css
+// let style = document.createElement('STYLE');
+// style.type = 'text/css';
+// style.innerHTML =
+//     '.sl-overlay {background: #FDE}; .sl-caption.pos-bottom {font-family: Koulen, cursive}';
+// document.querySelector('body').append(style);
