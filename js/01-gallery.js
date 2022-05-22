be01a256a623e14f0e097af2f0d396fa38a695d4
AnastasiiaLAV <lav21051991@gmail.com>
2022-05-22 15:06:30 +0300

    add first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    wrapGallery: document.querySelector("div.gallery"),
+}
+
+const markupGalleryItems = createsPictureCards(galleryItems);
+
+refs.wrapGallery.insertAdjacentHTML("beforeend", markupGalleryItems)
+
+refs.wrapGallery.addEventListener('click', openPicture)
+
+
+function createsPictureCards(items) {
+    return items.map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+        </div> `
+        })
+        .join('')
+
+}
+
+function openPicture(e) {
+
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+    e.preventDefault();
+
+    const instance = basicLightbox.create(
+        `<img src="${e.target.dataset.source}">`,
+    );
+    instance.show()
+
+    function closePicture(e) {
+
+        if (e.code === 'Escape') {
+            return instance.close();
+        }
+    }
+    window.addEventListener('keydown', closePicture);
+}
